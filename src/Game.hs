cb3d1550a59a45f1de47f7e68964ccc4f9fb67b2
Samuel Gélineau <gelisam+github@gmail.com>
2013-08-17 11:26:12 -0400

    Type-family-based generalization

--- src/Game.hs
+++ src/Game.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS -XTypeFamilies -XFlexibleContexts #-}
 module Game where
 
 import Control.Applicative
@@ -78,10 +79,66 @@ play (GameState minRow  player  board) (x, y) = g''
     board'  = set_at (x, y) (Just player) board
 
 
+class Game a where
+  player' :: a -> Player
+  winner' :: a -> Winner
+  
+  data GameIx a :: *
+  game_range' :: (GameIx a, GameIx a)
+  game_index' :: a -> GameIx a
+  indexed_game' :: GameIx a -> a
+  
+  data GameMove a :: *
+  legal_moves' :: a -> [GameMove a]
+  play' :: a -> GameMove a -> a
+
+type BestMove a = (Maybe (GameMove a), Winner, Int)
+
+
 -- the best move at each game state, for each player.
 -- True wants to delay the conclusion of the game, while
 -- False wants to precipitate it.
 -- Both want to win.
+best_moves' :: (Game a, Ix (GameIx a))
+            => Array (GameIx a) (BestMove a)
+best_moves' = array game_range' [(ix, f ix) | ix <- range game_range']
+  where
+    f = best_move' . indexed_game'
+
+best_move' :: (Game a, Ix (GameIx a))
+           => a
+           -> BestMove a
+best_move' g = case winner' g of
+                 Nothing -> best_from g
+                 Just p  -> (Nothing, Just p, 0)
+  where
+    best_from = maximumBy (compare `on` value) . (tie:) . map outcome . legal_moves'
+    tie = (Nothing, Nothing, 0)
+    outcome m = let (     _, winner, moves_left  ) = response m
+                 in (Just m, winner, moves_left+1)
+    value (Nothing, _, _)                          = -3000
+    value (_, winner, _) | winner == Just opponent = -2000
+    value (_, winner, _) | winner == Nothing       = -1000
+    value (_, _, moves_left) = if cur_player then moves_left else -moves_left
+    response = (best_moves' !) . game_index' . play' g
+    opponent = not cur_player
+    cur_player = player' g
+
+
+instance Game GameState where
+  player' = player
+  winner' = winner . board
+  
+  newtype GameIx GameState = GameStateIx' {runGameStateIx' :: GameStateIx}
+  game_range' = let (x, y) = game_range
+                 in (GameStateIx' x, GameStateIx' y)
+  game_index' = GameStateIx' . game_index
+  indexed_game' = indexed_game . runGameStateIx'
+  
+  newtype GameMove GameState = Move' {runMove' :: Move}
+  legal_moves' = map Move' . legal_moves
+  play' g = play g . runMove'
+
 best_moves :: Array GameStateIx (Maybe Move, Winner, Int)
 best_moves = array game_range $ map f $ range game_range
   where
