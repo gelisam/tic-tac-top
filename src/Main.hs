bee7eb5e0fa59b5df8a379e68ad77eaa31699297
Samuel Gélineau <gelisam+github@gmail.com>
2013-08-18 18:46:17 -0400

    generalize the console UI to any Playable Game
    ...where "Playable" and "Game" are both type classes.

--- src/Main.hs
+++ src/Main.hs
@@ -15,6 +15,35 @@ import TicTacTop
 import Play
 
 
+instance Playable TicTacTop where
+  markGame g = unlines . mark . lines where
+    mark xs = zipWith (++) xs marks
+    marks = replicate (minRow g) " (forbidden)" ++ repeat ""
+  
+  printGame = printBoard . board
+  
+  printChoices g = print_marked letterBoard
+    where
+      print_marked = markGame g . unlines
+      letterBoard = (map.map) dot_if_illegal letterGrid
+      dot_if_illegal c = if letterMove c `elem` legal_moves g
+                           then c
+                           else '.'
+  
+  validateChoice g [c] | c `elem` "123456789" = if letterMove c `elem` legal_moves g
+                                                  then Just (letterMove c)
+                                                  else Nothing
+  validateChoice g _ = Nothing
+
+letterGrid :: [[Char]]
+letterGrid = ["123","456","789"]
+
+letterMove :: Char -> GameMove TicTacTop
+letterMove = GameMove . head . matching_positions
+  where
+    matching_positions c = filter (\p -> letterGrid `at` p == c) positions
+
+
 main = do let b = indexed_board (fst board_range)
               g = TicTacTop 0 False b
           play_game g
--- src/Play.hs
+++ src/Play.hs
@@ -11,20 +11,21 @@ import Player
 import Board
 import Game
 import AI
-import TicTacTop
+
+
+class AI a => Playable a where
+  markGame :: a -> String -> String
+  printGame :: a -> String
+  printChoices :: a -> String
+  validateChoice :: a -> String -> Maybe (GameMove a)
 
 
 printd :: String -> IO ()
 printd s = do threadDelay 500000
               putStrLn s
 
-markBoard :: TicTacTop -> String -> String
-markBoard g = unlines . mark . lines where
-  mark xs = zipWith (++) xs marks
-  marks = replicate (minRow g) " (forbidden)" ++ repeat ""
-
-inspect_game :: TicTacTop -> IO ()
-inspect_game g = do putStrLn $ (if player g then markBoard g else id) $ printBoard b
+inspect_game :: Playable a => a -> IO ()
+inspect_game g = do putStrLn $ (if p then markGame g else id) $ printGame g
                     case winner g of
                       Nothing    -> return ()
                       Just True  -> do printd "The computer wins."
@@ -35,15 +36,15 @@ inspect_game g = do putStrLn $ (if player g then markBoard g else id) $ printBoa
                       printd "It's a tie."
                       exitSuccess
   where
-    b = board g
+    p = current_player g
 
-next_turn :: TicTacTop -> IO ()
+next_turn :: Playable a => a -> IO ()
 next_turn g = do inspect_game g
-                 if player g
+                 if current_player g
                    then ai_to_play g
                    else user_to_play g
 
-ai_to_play :: TicTacTop -> IO ()
+ai_to_play :: Playable a => a -> IO ()
 ai_to_play g = do when (winner == Just True && moves_left > 1) $ do
                     printd $ printf "I think I can win in %d moves.\n" (moves_left `quot` 2)
                   when (winner == Just False && moves_left == 2) $ do
@@ -56,40 +57,30 @@ ai_to_play g = do when (winner == Just True && moves_left > 1) $ do
   where
     (Just m, winner, moves_left) = best_move g
 
-user_to_play :: TicTacTop -> IO ()
+user_to_play :: Playable a => a -> IO ()
 user_to_play g = do when (winner == Just False && moves_left > 1) $ do
                       printd $ printf "You could win in %d moves.\n" ((moves_left + 1) `quot` 2)
                     when (winner == Just True && moves_left == 2) $ do
                       printd $ printf "Got you cornered!\n"
                     when (winner == Just True && moves_left == 1) $ do
                       printd $ printf "Checkmates!\n"
-                    putStr $ markBoard g $ unlines choice_grid
+                    putStr $ printChoices g
                     putStr "> "
                     hFlush stdout
                     line <- getLine
-                    let choice = if null line then '.' else head line
-                    case lookup choice choices of
+                    case validateChoice g line of
                       Nothing -> do printd "That is not a legal move.\n"
                                     user_to_play g
                       Just m -> do putStrLn ""
                                    let g' = play g m
-                                   when (minRow g' > 0) $ do
-                                     putStrLn $ printBoard $ board g'
+                                   let b' = printGame g'
+                                   when (markGame g' b' /= b') $ do
+                                     putStrLn $ b'
                                      threadDelay 500000
                                    next_turn g'
   where
-    b = board g
     (_, winner, moves_left) = best_move g
-    
-    full_choice_grid = ["123","456","789"]
-    choice_grid = (map.map) dot_if_invalid full_choice_grid
-    dot_if_invalid c = if valid_choice c then c
-                                         else cell_at c
-    valid_choice k = k `elem` map fst choices
-    choices = [(full_choice_grid `at` movePos m, m) | m <- legal_moves g]
-    cell_at = printCell . fromJust . flip lookup cell_map
-    cell_map = [(full_choice_grid `at` m, b `at` m) | m <- positions]
 
-play_game :: TicTacTop -> IO ()
+play_game :: Playable a => a -> IO ()
 play_game g = do printd "please wait while the computer evaluates your chances.\n"
                  next_turn g
