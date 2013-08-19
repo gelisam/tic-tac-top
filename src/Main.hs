dc18c0b2949528d707b46fead6f950937d160535
Samuel Gélineau <gelisam+github@gmail.com>
2013-08-18 22:46:14 -0400

    move TicTacTop-specific code to TicTacTop.hs

--- src/Main.hs
+++ src/Main.hs
@@ -15,39 +15,6 @@ import TicTacTop
 import Play
 
 
-instance Playable TicTacTop where
-  markGame g = unlines . mark . lines where
-    mark xs = zipWith (++) xs marks
-    marks = replicate (minRow g) " (forbidden)" ++ repeat ""
-  
-  printGame = printBoard . board
-  
-  printChoices g = print_marked choiceBoard
-    where
-      print_marked = markGame g . unlines
-      choiceBoard = (map.map) dot_if_illegal letterGrid
-      dot_if_illegal c = if letterMove c `elem` legal_moves g
-                           then c
-                           else letterBoard `at` letterPos c
-      letterBoard = (map.map) printCell (board g)
-  
-  validateChoice g [c] | c `elem` "123456789" = if letterMove c `elem` legal_moves g
-                                                  then Just (letterMove c)
-                                                  else Nothing
-  validateChoice g _ = Nothing
-
-letterGrid :: [[Char]]
-letterGrid = ["123","456","789"]
-
-letterPos :: Char -> Pos
-letterPos = head . matching_positions
-  where
-    matching_positions c = filter (\p -> letterGrid `at` p == c) positions
-
-letterMove :: Char -> GameMove TicTacTop
-letterMove = GameMove . letterPos
-
-
 main = do let b = indexed_board (fst board_range)
               g = TicTacTop 0 False b
           play_game g
--- src/TicTacTop.hs
+++ src/TicTacTop.hs
@@ -11,6 +11,7 @@ import Player
 import Board
 import Game
 import AI
+import Play
 
 
 data TicTacTop = TicTacTop
@@ -91,4 +92,37 @@ instance Game TicTacTop where
       player' = not player
       board'  = set_at (x, y) (Just player) board
 
-instance AI TicTacTop where
+
+instance AI TicTacTop
+
+instance Playable TicTacTop where
+  markGame g = unlines . mark . lines where
+    mark xs = zipWith (++) xs marks
+    marks = replicate (minRow g) " (forbidden)" ++ repeat ""
+  
+  printGame = printBoard . board
+  
+  printChoices g = print_marked choiceBoard
+    where
+      print_marked = markGame g . unlines
+      choiceBoard = (map.map) dot_if_illegal letterGrid
+      dot_if_illegal c = if letterMove c `elem` legal_moves g
+                           then c
+                           else letterBoard `at` letterPos c
+      letterBoard = (map.map) printCell (board g)
+  
+  validateChoice g [c] | c `elem` "123456789" = if letterMove c `elem` legal_moves g
+                                                  then Just (letterMove c)
+                                                  else Nothing
+  validateChoice g _ = Nothing
+
+letterGrid :: [[Char]]
+letterGrid = ["123","456","789"]
+
+letterPos :: Char -> Pos
+letterPos = head . matching_positions
+  where
+    matching_positions c = filter (\p -> letterGrid `at` p == c) positions
+
+letterMove :: Char -> GameMove TicTacTop
+letterMove = GameMove . letterPos
